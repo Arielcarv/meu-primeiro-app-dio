706f5c3c444d1c0fcd34240eb3612a33321f24de
Ariel Carvalho <arielcarvnasc@gmail.com>
2023-10-15 23:14:35 -0300

    feat: ElectricCarAPP - Getting EditText values with Kotlin.

--- app/src/main/java/com/example/electriccarapp/MainActivity.kt
+++ app/src/main/java/com/example/electriccarapp/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.electriccarapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+    lateinit var chargePrice: EditText
+    lateinit var btnCalculate: Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupViews()
+        setupListeners()
+    }
+
+    fun setupViews() {
+        chargePrice = findViewById<EditText>(R.id.et_charge_price)
+        btnCalculate = findViewById<Button>(R.id.btn_calculate)
+    }
+
+    fun setupListeners() {
+        btnCalculate.setOnClickListener {
+            val typedText = chargePrice.text.toString()
+            Log.d("Typed Text -> ", typedText)
+        }
     }
 }
