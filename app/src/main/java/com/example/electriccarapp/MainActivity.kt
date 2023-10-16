52f8fb42cd4ae44e7b63271bc100d7e943717ea4
Ariel Carvalho <arielcarvnasc@gmail.com>
2023-10-16 17:43:58 -0300

    feat: ElectricCarAPP - Manipulate values and show on screen.

--- app/src/main/java/com/example/electriccarapp/MainActivity.kt
+++ app/src/main/java/com/example/electriccarapp/MainActivity.kt
@@ -2,13 +2,15 @@ package com.example.electriccarapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     lateinit var chargePrice: EditText
     lateinit var btnCalculate: Button
+    lateinit var kmsDriven: EditText
+    lateinit var automonyResult: TextView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,12 +24,21 @@ class MainActivity : AppCompatActivity() {
     fun setupViews() {
         chargePrice = findViewById<EditText>(R.id.et_charge_price)
         btnCalculate = findViewById<Button>(R.id.btn_calculate)
+        kmsDriven = findViewById<EditText>(R.id.et_kms_driven)
+        automonyResult = findViewById<TextView>(R.id.tv_autonomy_result)
     }
 
     fun setupListeners() {
         btnCalculate.setOnClickListener {
-            val typedText = chargePrice.text.toString()
-            Log.d("Typed Text -> ", typedText)
+            calculateCarAutonomy()
         }
+    }
+
+    fun calculateCarAutonomy() {
+        val chargePrice = chargePrice.text.toString().toFloat()
+        val kmsDriven = kmsDriven.text.toString().toFloat()
+        val autonomy = chargePrice / kmsDriven
+
+        automonyResult.text = autonomy.toString()
     }
 }
