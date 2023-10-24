9b39e4045993ebbc785a42bffc5c359ae209a3e0
Ariel Carvalho <arielcarvnasc@gmail.com>
2023-10-23 22:42:23 -0300

    refactor: ElectricCarAPP - implement ListView.

--- app/src/main/java/com/example/electriccarapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/electriccarapp/presentation/MainActivity.kt
@@ -3,11 +3,14 @@ package com.example.electriccarapp.presentation
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.ListView
 import com.example.electriccarapp.R
 
 class MainActivity : AppCompatActivity() {
     lateinit var btnRedirect: Button
+    lateinit var cars_list: ListView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,11 +18,28 @@ class MainActivity : AppCompatActivity() {
 
         setupViews()
         setupListeners()
+        setupList()
     }
 
     fun setupViews() {
         btnRedirect = findViewById<Button>(R.id.btn_redirect)
+        cars_list = findViewById<ListView>(R.id.lv_infos)
     }
+
+    fun setupList() {
+        var data = arrayOf(
+            "Cupcake",
+            "Donut",
+            "Froyo",
+            "Gingerbread",
+            "Honeycomb",
+            "Ice Cream Sandwich",
+            "Jelly Bean"
+        )
+        var adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, data)
+        cars_list.adapter = adapter
+    }
+
 
     fun setupListeners() {
         btnRedirect.setOnClickListener {
