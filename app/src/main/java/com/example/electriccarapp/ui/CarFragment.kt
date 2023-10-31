b20e8a5a7f7ebadaa9dcc2cccbc1726ac3809c29
Ariel Carvalho <arielcarvnasc@gmail.com>
2023-10-31 16:05:40 -0300

    refactor: ElectricCarAPP - Refactoring fragment Views.

--- app/src/main/java/com/example/electriccarapp/ui/CarFragment.kt
+++ app/src/main/java/com/example/electriccarapp/ui/CarFragment.kt
@@ -1,18 +1,49 @@
 package com.example.electriccarapp.ui
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import com.example.electriccarapp.R
+import com.example.electriccarapp.data.CarFactory
+import com.example.electriccarapp.ui.adapter.CarAdapter
 
 class CarFragment : Fragment() {
+    private lateinit var btnCalculator: Button
+    private lateinit var carsList: RecyclerView
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.car_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        setupViews(view)
+        setupList()
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    private fun setupViews(view: View) {
+        view.apply {
+            btnCalculator = findViewById(R.id.btn_calculator)
+            carsList = findViewById(R.id.rv_cars_list)
+        }
+    }
+    private fun setupList() {
+        val data = CarFactory.list
+        val adapter = CarAdapter(data)
+        carsList.adapter = adapter
+    }
+
+    private fun setupListeners() {
+        btnCalculator.setOnClickListener {
+//            startActivity(Intent(this, CalculateAutonomyActivity::class.java))
+        }
     }
 }
--- app/src/main/java/com/example/electriccarapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/electriccarapp/ui/MainActivity.kt
@@ -1,58 +1,31 @@
 package com.example.electriccarapp.ui
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
-import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.ViewPager2
 import com.example.electriccarapp.R
-import com.example.electriccarapp.data.CarFactory
-import com.example.electriccarapp.ui.adapter.CarAdapter
 import com.example.electriccarapp.ui.adapter.TabAdapter
 import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var btnCalculator: Button
-    private lateinit var carsList: RecyclerView
     private lateinit var tabLayout: TabLayout
     private lateinit var viewPager2: ViewPager2
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.d("Android LifeCycle", "CREATE")
-
         setContentView(R.layout.activity_main)
         setupViews()
-        setupListeners()
-        setupList()
         setupTabs()
     }
 
     private fun setupViews() {
         tabLayout = findViewById(R.id.tab_layout)
-        btnCalculator = findViewById(R.id.btn_calculator)
-        carsList = findViewById(R.id.rv_cars_list)
         viewPager2 = findViewById(R.id.vp_view_pager)
-    }
-
-    private fun setupList() {
-        val data = CarFactory.list
-        val adapter = CarAdapter(data)
-        carsList.adapter = adapter
     }
 
     private fun setupTabs() {
         val tabsAdapter = TabAdapter(this)
         viewPager2.adapter = tabsAdapter
-    }
-
-    private fun setupListeners() {
-        btnCalculator.setOnClickListener {
-            startActivity(Intent(this, CalculateAutonomyActivity::class.java))
-        }
-
         tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
             override fun onTabSelected(tab: TabLayout.Tab?) {
                 tab?.let {
@@ -61,13 +34,18 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onTabUnselected(tab: TabLayout.Tab?) {
-                TODO("Not yet implemented")
+//                TODO("Not yet implemented")
             }
 
             override fun onTabReselected(tab: TabLayout.Tab?) {
-                TODO("Not yet implemented")
+//                TODO("Not yet implemented")
             }
         })
-
+        viewPager2.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                tabLayout.getTabAt(position)?.select()
+            }
+        })
     }
 }
