945743aee046fa6eb2106c7e8f0418578217d5a8
Ariel Carvalho <arielcarvnasc@gmail.com>
2023-11-09 22:04:37 -0300

    refactor: ElectricCarAPP - refactoring requests to Retrofit.

--- app/src/main/java/com/example/electriccarapp/ui/CarFragment.kt
+++ app/src/main/java/com/example/electriccarapp/ui/CarFragment.kt
@@ -7,32 +7,26 @@ import android.net.NetworkCapabilities
 import android.net.NetworkInfo
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
+import android.widget.Toast
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.example.electriccarapp.R
+import com.example.electriccarapp.data.CarsAPI
 import com.example.electriccarapp.domain.Car
 import com.example.electriccarapp.ui.adapter.CarAdapter
 import com.google.android.material.floatingactionbutton.FloatingActionButton
-import kotlinx.coroutines.DelicateCoroutinesApi
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import org.json.JSONArray
-import org.json.JSONTokener
-import java.io.BufferedReader
-import java.io.IOException
-import java.io.InputStreamReader
-import java.net.HttpURLConnection
-import java.net.URL
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class CarFragment : Fragment() {
     private lateinit var fabCalculator: FloatingActionButton
@@ -40,7 +34,7 @@ class CarFragment : Fragment() {
     private lateinit var progressWidget: ProgressBar
     private lateinit var noInternetImage: ImageView
     private lateinit var noInternetText: TextView
-    private var carsArray: ArrayList<Car> = ArrayList()
+    private lateinit var carsApi: CarsAPI
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
@@ -50,6 +44,7 @@ class CarFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupRetrofit()
         setupViews(view)
         setupListeners()
     }
@@ -57,10 +52,38 @@ class CarFragment : Fragment() {
     override fun onResume() {
         super.onResume()
         if (checkInternetConnectivity(requireContext())) {
-            callService()
+            getAllCars()
         } else {
             emptyState()
         }
+    }
+
+    fun setupRetrofit() {
+        val builder = Retrofit.Builder()
+            .baseUrl("https://igorbag.github.io/cars-api/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        carsApi = builder.create(CarsAPI::class.java)
+    }
+
+    fun getAllCars() {
+        carsApi.getAllCars().enqueue(object : Callback<List<Car>> {
+            override fun onResponse(call: Call<List<Car>>, response: Response<List<Car>>) {
+                if (response.isSuccessful) {
+                    progressWidget.visibility = View.GONE
+                    noInternetImage.visibility = View.GONE
+                    noInternetText.visibility = View.GONE
+                    response.body()?.let { setupList(it) }
+                } else {
+                    Toast.makeText(context, R.string.response_error, Toast.LENGTH_LONG).show()
+                }
+            }
+
+            override fun onFailure(call: Call<List<Car>>, t: Throwable) {
+                Toast.makeText(context, R.string.response_error, Toast.LENGTH_LONG).show()
+            }
+        })
     }
 
     private fun emptyState() {
@@ -80,7 +103,7 @@ class CarFragment : Fragment() {
         }
     }
 
-    private fun setupList() {
+    private fun setupList(carsArray: List<Car>) {
         val carAdapter = CarAdapter(carsArray)
         carsList.apply {
             visibility = View.VISIBLE
@@ -91,74 +114,6 @@ class CarFragment : Fragment() {
     private fun setupListeners() {
         fabCalculator.setOnClickListener {
             startActivity(Intent(context, CalculateAutonomyActivity::class.java))
-        }
-    }
-
-    @OptIn(DelicateCoroutinesApi::class)
-    private fun callService() {
-        progressWidget.visibility = View.VISIBLE
-        GlobalScope.launch(Dispatchers.IO) {
-            try {
-                val json = getUrlData("https://igorbag.github.io/cars-api/cars.json")
-                processJsonData(json)
-            } catch (ex: Exception) {
-                Log.e("Error", "Error fetching data: ${ex.message}")
-            }
-        }
-    }
-
-    private fun getUrlData(urlString: String): String {
-        val url = URL(urlString)
-        val connection = url.openConnection() as HttpURLConnection
-        connection.readTimeout = 6000
-        connection.connectTimeout = 6000
-        connection.setRequestProperty("Accept", "application/json")
-        return try {
-            if (connection.responseCode == HttpURLConnection.HTTP_OK) {
-                val reader = BufferedReader(InputStreamReader(connection.inputStream))
-                val response = StringBuilder()
-                var line: String?
-                while (reader.readLine().also { line = it } != null) {
-                    response.append(line)
-                }
-                response.toString()
-            } else {
-                throw IOException("HTTP Error: ${connection.responseCode}")
-            }
-        } finally {
-            connection.disconnect()
-        }
-    }
-
-    private suspend fun processJsonData(jsonData: String) {
-        withContext(Dispatchers.Main) {
-            try {
-                val jsonArray = JSONTokener(jsonData).nextValue() as JSONArray
-                for (i in 0 until jsonArray.length()) {
-                    val id = jsonArray.getJSONObject(i).getString("id")
-                    val price = jsonArray.getJSONObject(i).getString("preco")
-                    val battery = jsonArray.getJSONObject(i).getString("bateria")
-                    val power = jsonArray.getJSONObject(i).getString("potencia")
-                    val charge = jsonArray.getJSONObject(i).getString("recarga")
-                    val photoUrl = jsonArray.getJSONObject(i).getString("urlPhoto")
-                    val carsModel = Car(
-                        id = id.toInt(),
-                        price = price,
-                        battery = battery,
-                        power = power,
-                        charge = charge,
-                        photoUrl = photoUrl
-                    )
-                    carsArray.add(carsModel)
-                    Log.d("Model -> ", carsModel.toString())
-                }
-                progressWidget.visibility = View.GONE
-                noInternetImage.visibility = View.GONE
-                noInternetText.visibility = View.GONE
-                setupList()
-            } catch (ex: Exception) {
-                Log.e("Error", "Error processing JSON data: ${ex.message}")
-            }
         }
     }
 
