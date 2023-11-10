a0e67edd8dbdfe1182196781844f6e28f4aae6b6
Ariel Carvalho <arielcarvnasc@gmail.com>
2023-11-10 19:06:05 -0300

    fix: ElectricCarAPP - correct CarModelClass to accept pt-br serializable attributes.

--- app/src/main/java/com/example/electriccarapp/domain/Car.kt
+++ app/src/main/java/com/example/electriccarapp/domain/Car.kt
@@ -1,10 +1,18 @@
 package com.example.electriccarapp.domain
 
+import com.google.gson.annotations.SerializedName
+
 class Car(
+    @SerializedName("id")
     val id: Int,
+    @SerializedName("preco")
     val price: String,
+    @SerializedName("bateria")
     val battery: String,
+    @SerializedName("potencia")
     val power: String,
+    @SerializedName("recarga")
     val charge: String,
-    val photoUrl: String,
+    @SerializedName("urlPhoto")
+    val photoUrl: String
 )
