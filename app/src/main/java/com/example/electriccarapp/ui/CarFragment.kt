7b52397b2cd0eed78ed3f12aa1d5c581efbd2cde
Ariel Carvalho <arielcarvnasc@gmail.com>
2023-11-24 14:47:42 -0300

    refactor: ElectricCarAPP - improve favorites listing Star.

--- app/src/main/java/com/example/electriccarapp/ui/CarFragment.kt
+++ app/src/main/java/com/example/electriccarapp/ui/CarFragment.kt
@@ -4,8 +4,6 @@ import android.content.Context
 import android.content.Intent
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
-import android.net.NetworkInfo
-import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -59,14 +57,14 @@ class CarFragment : Fragment() {
         }
     }
 
-    fun setupRetrofit() {
+    private fun setupRetrofit() {
         val builder = Retrofit.Builder().baseUrl("https://igorbag.github.io/cars-api/")
             .addConverterFactory(GsonConverterFactory.create()).build()
 
         carsApi = builder.create(CarsAPI::class.java)
     }
 
-    fun getAllCars() {
+    private fun getAllCars() {
         carsApi.getAllCars().enqueue(object : Callback<List<Car>> {
             override fun onResponse(call: Call<List<Car>>, response: Response<List<Car>>) {
                 if (response.isSuccessful) {
@@ -104,6 +102,7 @@ class CarFragment : Fragment() {
 
     private fun setupList(carsArray: List<Car>) {
         val carAdapter = CarAdapter(carsArray)
+//        val carsArrayFromLocalDatabase = getCarsOnLocalDB()
         carsList.apply {
             visibility = View.VISIBLE
             adapter = carAdapter
@@ -123,19 +122,18 @@ class CarFragment : Fragment() {
         val connectivityManager =
             context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            val network = connectivityManager.activeNetwork ?: return false
-            val activeNetwork = connectivityManager.getNetworkCapabilities(network) ?: return false
+        val network = connectivityManager.activeNetwork ?: return false
+        val activeNetwork = connectivityManager.getNetworkCapabilities(network) ?: return false
 
-            return when {
-                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
-                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
-                else -> false
-            }
-        } else {
-            @Suppress("DEPRECATION") val networkInfo: NetworkInfo =
-                connectivityManager.activeNetworkInfo ?: return false
-            @Suppress("DEPRECATION") return networkInfo.isConnected
+        return when {
+            activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+            activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+            else -> false
         }
+    }
+
+    private fun getCarsOnLocalDB(): List<Car> {
+        val carRepository = CarRepository(requireContext())
+        return carRepository.getAll()
     }
 }
